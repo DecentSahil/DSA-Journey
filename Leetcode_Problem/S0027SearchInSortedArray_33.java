7be2c385d7abde51f2db4dd602762cb201e5c882
DecentSahil <168282363+DecentSahil@users.noreply.github.com>
2026-01-21 00:40:53 +0530

    Simple Binary Search Beats 100%😎😎

---
+++ Leetcode_Problem/S0027SearchInSortedArray_33.java
@@ -0,0 +1,29 @@
+// Date : 21-01-2026
+class Solution {
+    public int search(int[] arr, int target) {
+        int n = arr.length;
+        int left = 0;
+        int right = arr.length-1;
+        while(left<=right){
+            int mid = left +(right-left)/2;
+            if(arr[mid]==target){
+                return mid;
+            }
+            else if(arr[left]<=arr[mid]){
+                if(target<arr[mid] && target>=arr[left]){
+                    right = mid-1;
+                }
+                else{
+                    left = mid+1;
+                }
+            } else{
+                if(target>arr[mid] && target<=arr[right]){
+                    left = mid+1;
+                }else{
+                    right = mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+}
