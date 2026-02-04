ccf72856a01b4e0f3a376d26afcbdb531f4b1dd9
DecentSahil <168282363+DecentSahil@users.noreply.github.com>
2026-02-04 22:05:59 +0530

    slow and fast pointer concept Beats 100%

---
+++ Leetcode_Problem/S0047MiddleOfLinkedList_876.java
@@ -0,0 +1,12 @@
+// Date: 04-02-2026
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast!=null && fast.next!=null ){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
