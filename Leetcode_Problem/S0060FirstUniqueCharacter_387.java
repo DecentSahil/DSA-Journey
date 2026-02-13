da33d757b88d82be400dc45ab3e15d9dc87f96b5
DecentSahil <168282363+DecentSahil@users.noreply.github.com>
2026-02-13 23:10:43 +0530

    String

---
+++ Leetcode_Problem/S0060FirstUniqueCharacter_387.java
@@ -0,0 +1,21 @@
+// Date: 13-02-2026
+class Solution {
+    public int firstUniqChar(String s) {
+        for (int i = 0; i < s.length(); i++) {
+            boolean isUnique = true;
+
+            for (int j = 0; j < s.length(); j++) {
+                if (i != j && s.charAt(i) == s.charAt(j)) {
+                    isUnique = false;
+                    break;
+                }
+            }
+
+            if (isUnique) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
